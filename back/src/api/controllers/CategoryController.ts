7e3f85e5781696e859d601cdead85b2ec2595ac4
Khrystyna Marchuk <christinamitrich@gmail.com>
2022-11-28 23:36:37 +0200

    add category controller

--- back/src/api/controllers/CategoryController.ts
+++ back/src/api/controllers/CategoryController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Repository } from  'typeorm';
+import { Repository, Like } from  'typeorm';
 import connection from "../../../db_connection";
 import Categories from '../models/Categories'
 import { validationResult } from 'express-validator';
@@ -13,7 +13,45 @@ class CategoryController {
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        const repo: Repository<Categories> = connection.getRepository(Categories);
+
+        try {
+            const repo: Repository<Categories> = connection.getRepository(Categories);
+
+            const {
+                categoryName,
+                categoryDescr
+            } = req.body;
+
+            const category = await repo.findOne( { where:
+                { category_name: Like(`%${categoryName}%`) }
+            });
+
+            if (!category) {
+                const requestData: {
+                    categoryName: string,
+                    categoryDescr: string,
+                } = {
+                    categoryName,
+                    categoryDescr
+                };
+                const newCategory = await repo.create({
+                    category_name: requestData.categoryName,
+                    category_descr: requestData.categoryDescr
+                });
+
+                newCategory.save();
+
+                return res.status(200).json({
+                    succes: true,
+                    msg: 'Category has been succesfully created',
+                    data: newCategory
+                });
+            } else {
+                return res.status(200).json("Category has already created");
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
     }
 }
 export default new CategoryController();
