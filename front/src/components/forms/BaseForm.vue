6b12ec8fbb36c616c44dd97dc61508c95f957321
Khrystyna Marchuk <christinamitrich@gmail.com>
2022-11-28 20:23:32 +0200

    add product form

---
+++ front/src/components/forms/BaseForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="form">
+        <div class="form-group">
+        <label for="product">Product name</label>
+        <input type="text" class="form-control" id="product" placeholder="Enter product name">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        form: {
+            type: Array,
+            default: []
+        }
+    }
+}
+</script>
