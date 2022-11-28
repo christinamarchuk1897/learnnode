b8400b9e84537a23c1cf48f076964f3b8af59dad
Khrystyna Marchuk <christinamitrich@gmail.com>
2022-11-28 21:01:16 +0200

    add product form

--- front/src/components/forms/BaseForm.vue
+++ front/src/components/forms/BaseForm.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="form">
-        <div class="form-group">
-        <label for="product">Product name</label>
-        <input type="text" class="form-control" id="product" placeholder="Enter product name">
+        <div v-for="(item, key) in form" :key="key">
+            <div class="form-group" v-if="item.type == types.input">
+                <label :for="item.id">{{ item.label }}</label>
+                <input  :type="item.type" class="form-control" :id="item.id" :placeholder="`Enter ${item.label}`">
+            </div>
         </div>
     </div>
 </template>
 <script>
+const TYPE_INPUT = 'input';
 export default {
     data() {
         return {
+            types: {
+                input: 'input'
+            },
 
+            form: {
+
+            }
         }
     },
+
     props: {
         form: {
             type: Array,
